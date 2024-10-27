76ea4b764f008d665c720a591e22c5c5c0062b95
Fábio Dias <fabiojoaodias@gmail.com>
2024-10-27 23:57:52 +0000

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,13 +94,13 @@ export default function RootLayout({
   return (
     <html lang="pt">
     <Head>
-          <!-- Google Tag Manager -->
+          
       <Script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-TCW6GLCH');</Script>
-      <!-- End Google Tag Manager -->
+      
     
       <meta name="yandex-verification" content="6015463b94fe0d61" />
       <link rel="icon" href="@/favicon180.png" sizes="any" />
@@ -122,10 +122,10 @@ export default function RootLayout({
         `}
       </Script>
       <body className={poppins.className}>
-      <!-- Google Tag Manager (noscript) -->
+      
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCW6GLCH"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+        
         <div className=" bg-gradient-to-r from-main to-main">
         < Nav />
         <Suspense  fallback={<LoadingSpinner />}>
